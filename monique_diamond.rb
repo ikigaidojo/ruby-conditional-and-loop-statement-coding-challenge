4a02c53886f1bcc00216b16d10dc6bb1da7a2487
moniquemanansala29 <moniquemanansala29@gmail.com>
2018-03-14 03:11:30 -0400

    diamond exercise

---
+++ monique_diamond.rb
@@ -0,0 +1,38 @@
+require File.dirname(__FILE__) + "/monique_diamond.rb"
+
+# this enable this ruby script to be run from the command output. Example - $> ruby diamond.rb
+if __FILE__ == $PROGRAM_NAME
+  MoniqueDiamond.new.main 
+end
+
+class MoniqueDiamond
+	
+
+	# place your code here...
+	def main
+	  print "Enter the value: "
+	  size = gets.chomp.to_i
+	  puts " "
+
+	for row in 1..size do
+	  for col in 1..size - row do
+	    print " "
+	  end
+	for star in 1..row do
+	   print "* "
+	  end
+	  puts "\n"
+	end
+
+	for row in (size - 1).downto 1 do
+	  for col in (size - row).downto 1 do
+	    print " "
+	  end
+	  for star in row.downto 1 do
+	    print "* "
+	  end
+	  puts "\n"
+	  end
+	end
+     end
+
